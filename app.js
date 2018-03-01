697b38d701efa207c19654575a4d568a44bb32a2
Akash Gajjar <akashgajjar8@gmail.com>
2018-03-01 17:18:03 +0530

    added logging and time tracking functionality

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var admin = require('firebase-admin');
 var serviceAccount = require('./serviceAccountKey.json');
 const express = require('express');
 const bodyParser = require('body-parser');
+var date = new Date();
+var inception = date.toUTCString();
+var mili = Date.now();
 
 admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount),
-   databaseURL: "https://amnesia-alarm.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://amnesia-alarm.firebaseio.com"
 });
 
 var database = admin.database();
@@ -14,10 +17,19 @@ var app = express();
 
 var user = database.ref('users/');
 
-function writeUserData(name,mobile){
-    database.ref('users/'+name).set({
+database.ref().child('onTime').set(inception);
+
+function writeUserData(name, mobile) {
+    database.ref('users/' + name).set({
+        fname: name.split(' ')[0],
         mobile: mobile,
-        failed: 0
+        failed: 0,
+    });
+}
+
+function updateUserData(name, mobile) {
+    database.ref('users/' + name).update({
+        mobile:mobile
     });
 }
 
@@ -26,25 +38,36 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(function(req, res, next) {
-  console.log(`${req.method} request for ${req.url} - ${JSON.stringify(req.body)}`);
-  next();
+app.get('/time',function(req,res,next){
+    res.json({
+        mili: mili
+    });
+});
+
+app.use(function (req, res, next) {
+    console.log(`${req.method} request for ${req.url} - ${JSON.stringify(req.body)}`);
+    next();
 });
 
 app.use(express.static("./public"));
 
-app.post('/users',function(req,res){
+app.post('/users', function (req, res) {
     console.log(req.body.name);
-    database.ref('/users/'+req.body.name).once('value',function(data){
+    database.ref('/users/' + req.body.name).once('value', function (data) {
         var value = data.val();
-        if(value == null){
-            console.log('the user does not exist!');
-            writeUserData(req.body.name,req.body.mobile);
-        }
+        if(value==null)  writeUserData(req.body.name, req.body.mobile);
+        else updateUserData(req.body.name,req.body.mobile);
     });
-    res.json({
-        success:true
-    });
+    
+    setTimeout(function(){
+        database.ref('/users/' + req.body.name).once('value', function (data) {
+            res.json(data);
+        });
+    },500);
+
+    var timeout = setInterval(function(){
+        database.ref('/users/' + req.body.name).child('log').push(Math.random());
+    },10000);
 });
 
 app.listen(8080);
--- public/script.js
+++ public/script.js
@@ -1,9 +1,27 @@
-console.log('script loaded!');
+$.ajax({
+    url:'/time',
+    success:function(data){
+        var hours = (Date.now()-data.mili)/3600000;
+        var mins = hours * 60;
+        $('.time').html("Running Since "+Math.floor(hours) +" hours");
+    }
+});
 
-$('form').on('submit',function (){
+$('.my-form').on('submit',function (){
     console.log('the form was submitted!');
     $.post($(this).attr('action'), $(this).serialize(), function(json) {
-        console.log(json);
+        $('.greeting').html("Hello, "+json.fname +"!");
+        var log = '';
+        json.log.forEach(function(data){
+            log+=data +'\n';
+        });
+        var html = `
+        <div class='log'>
+            <p class='log-header'>Log:</p>
+            <p class='log-info'>${log}</p>
+        </div>
+        `;
+        $('.main').html(html);
       }, 'json');
     return false;
 });
